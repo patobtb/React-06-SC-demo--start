178ec3f626bc1656e73eb7f0472f0156cc04e17c
Pato Brugmann <patobrugmann@gmail.com>
2024-05-20 11:43:57 +0300

    install styled-components and refactor the TopBar component to use it

--- src/view/TopBar.jsx
+++ src/view/TopBar.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import styled, {keyframes} from "styled-components";
+
 
 const TopBar =  ({ children }) => (
-  <div className="top-bar">
+  <Div className="top-bar">
     <h1>{children}</h1>
     <img className="logo" src="icons/logo.svg" alt="logo" />
-  </div>
+  </Div>
 );
 export default TopBar;
+
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Div = styled.div`
+  background: slategray;
+  width: 100%;
+  height: 70px;
+  padding: 20px;
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  
+  h1{
+    font-size: 3rem;
+    font-weight: normal;
+    font-family: "Griffy", cursive;
+    color: papayawhip;
+  }
+
+  img.logo{
+    height: 5rem;
+    animation: ${spin} 2s linear infinite;
+  }
+`;
+
